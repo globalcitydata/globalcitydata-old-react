7d3de8975795e6ad6957d1192717e9b5d4eb17b4
Rafi Barash <rafabarash@gmail.com>
2018-07-26 01:43:08 -0500

    hover works

--- src/views/Home/Sections/QueryBar.js
+++ src/views/Home/Sections/QueryBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Input, Button, Col, Dropdown } from 'react-materialize';
+import ReactTooltip from 'react-tooltip';
 import { tagState } from '../../../utils/data';
 import { query } from '../../../utils/searchAndFilter';
 import { tags } from '../../../utils/tags';
@@ -16,33 +17,41 @@ class QueryBar extends React.Component {
   handleDropdown = e => console.log(e.target);
 
   handleSubmit = e => {
+    e.preventDefault();
     const value = this.state;
     const { selectedOptions } = this.state;
     const { dataList, updateDataList } = this.props;
-
     for (const tag of selectedOptions) {
       value[tag.group][tag.value] = true;
     }
-
     let filteredData;
     if (selectedOptions.length == 0) filteredData = dataList;
     else {
       filteredData = query(dataList, value);
       if (!filteredData) filteredData = [];
     }
-
     updateDataList(filteredData);
-
-    // should clear query state at this point
-    e.preventDefault();
   };
+
   render() {
-
-
     console.log(tags);
-
     return (
       <div className="queryBarContainer">
+        <ReactTooltip id='parameters' effect='solid'>
+          <ul>
+            <li>- Social & economic (population demographics, economy, households, policy, etc)</li>
+            <li>- Environmental characteristics (soil, temperature, vegetation, etc)</li>
+            <li>- Infrastructure & food supply (Supply of energy, water, transportation, etc)</li>
+            <li>- Urban form (density, spatial layout)</li>
+          </ul>
+        </ReactTooltip>
+        <ReactTooltip id='spatialScales' effect='solid'>
+          <ul>
+            <li>- Intra-urban (fine spatial detail within a city, census tract, block group or finer)</li>
+            <li>- Whole City (aggregate data for one or more cities)</li>
+            <li>- National Urban (aligned data for all cities or urban areas in a country)</li>
+          </ul>
+        </ReactTooltip>
         <Row className="blue">
           <Col>
             <Dropdown trigger={<Button onClick={this.handleDropdown}>Content Type</Button>}>
@@ -54,7 +63,7 @@ class QueryBar extends React.Component {
           </Col>
 
           <Col>
-            <Dropdown trigger={<Button onClick={this.handleDropdown}>Parameters</Button>}>
+            <Dropdown trigger={<Button onClick={this.handleDropdown} data-tip data-for='parameters'> Parameters</Button>}>
               <Row className="queryDropdownOptions">
                 <Input s={12} name='group1' type='checkbox' value='red' label='Red' />
                 <Input s={12} name='group1' type='checkbox' value='yellow' label='Yellow' defaultValue='checked' />
@@ -72,7 +81,7 @@ class QueryBar extends React.Component {
           </Col>
 
           <Col>
-            <Dropdown trigger={<Button onClick={this.handleDropdown}>Spatial Scales</Button>}>
+            <Dropdown trigger={<Button onClick={this.handleDropdown} data-tip data-for='spatialScales'>Spatial Scales</Button>}>
               <Row className="queryDropdownOptions">
                 <Input s={12} name='group1' type='checkbox' value='red' label='Red' />
                 <Input s={12} name='group1' type='checkbox' value='yellow' label='Yellow' defaultValue='checked' />
@@ -102,8 +111,8 @@ class QueryBar extends React.Component {
             <Button className="querySearch">Search</Button>
           </Col>
 
-        </Row>
-      </div>
+        </Row >
+      </div >
       // <div className="queryBar">
       //   <Row>
       //     <Col s={12} m={10}>
