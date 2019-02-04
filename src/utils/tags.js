34fe94650bb8075d8600734f302bc1c87d4d280e
Rafael Barash <rafabarash@gmail.com>
2019-02-04 14:44:05 -0600

    refractored /src/utils/tags.js

--- src/utils/tags.js
+++ src/utils/tags.js
@@ -1,20 +1,42 @@
 import { startCase } from 'lodash';
 
-export const CONTENT_TYPES = ['dataset', 'model', 'tutorial'];
-export const OUTCOMES = [
-  'environmentalImpacts',
-  'equity',
-  'health',
-  'livability',
-  'wellBeing',
-];
-export const PARAMETERS = [
+// FOLLOWING TAGS ARE VERY IMPORTANT AND USED THROUGHOUT APP
+// ONLY CHANGE IF YOU 110% MUST CHANGE
+// These are used to filter in the queryBar
+// These are associated with the tags at the end of the dataState in /src/utils/data.js
+// These are exported to the dataSubmit form in /src/views/dataSubmit/
+
+// This tag not used in query bar but still used in data submit form
+export const DATA_TYPES = ['dataset', 'model', 'tutorial'];
+export const DETERMINANTS = [
   'socialAndEconomic',
   'environmentalCharacteristics',
   'infrastructureAndFoodSupply',
   'urbanForm',
+  'policy',
+  'humanBehavior',
+  'investments',
+];
+export const SECTORS = [
+  'buildings',
+  'energy',
+  'transportation',
+  'informationCommunication',
+  'waterSupply',
+  'sanitationAndWaste',
+  'foodSupply',
+  'landAndPublicSpace',
 ];
 export const SPATIAL_SCALES = ['intraUrban', 'wholeCity', 'nationalUrban'];
+export const SUSTAINABILITY_OUTCOMES = [
+  'environment',
+  'equity',
+  'health',
+  'livability',
+  'wellBeing',
+  'economy',
+  'resilience',
+];
 export const TEMPORAL_SCALES = [
   'singleYearSnapshot',
   'timeSeries',
@@ -28,58 +50,55 @@ export const WORLD_REGIONS = [
   'asia',
 ];
 
-const typeOptions = CONTENT_TYPES.map(t => ({
+// USED TO GROUP TAGS FOR QUERYBAR AND DATASUBMIT FORM
+const determinantOptions = DETERMINANTS.map(t => ({
   value: t,
   label: startCase(t),
-  group: 'contentType',
+  group: 'determinants',
 }));
-
-const outcomeOptions = OUTCOMES.map(t => ({
+const sectorOptions = SECTORS.map(t => ({
   value: t,
   label: startCase(t),
-  group: 'outcomes',
+  group: 'sectors',
 }));
-
-const parameterOptions = PARAMETERS.map(t => ({
-  value: t,
-  label: startCase(t),
-  group: 'parameters',
-}));
-
 const spatialOptions = SPATIAL_SCALES.map(t => ({
   value: t,
   label: startCase(t),
   group: 'spatialScales',
 }));
-
+const sustainabilityOptions = SUSTAINABILITY_OUTCOMES.map(t => ({
+  value: t,
+  label: startCase(t),
+  group: 'sustainabilityOutcomes',
+}));
 const temporalOptions = TEMPORAL_SCALES.map(t => ({
   value: t,
   label: startCase(t),
   group: 'temporalScales',
 }));
-
 const worldOptions = WORLD_REGIONS.map(t => ({
   value: t,
   label: startCase(t),
   group: 'worldRegions',
 }));
 
+// ACTUAL TAGS BEING EXPORTED TO QUERYBAR FOR FILTERING
 export const tags = [
   {
-    label: 'Content Type',
-    options: typeOptions,
+    label: 'Determinants',
+    options: determinantOptions,
   },
   {
-    label: 'Outcomes',
-    options: outcomeOptions,
-  },
-  {
-    label: 'Parameters',
-    options: parameterOptions,
+    label: 'Sectors',
+    options: sectorOptions,
   },
   {
     label: 'Spatial Scales',
     options: spatialOptions,
+  },
+  {
+    label: 'Sustainability Outcomes',
+    options: sustainabilityOptions,
   },
   {
     label: 'Temporal Scales',
@@ -91,11 +110,12 @@ export const tags = [
   },
 ];
 
+// ALSO EXPORTED TO QUERYBAR FOR FILTERING
 export const tagQuery = {
-  contentType: [],
-  outcomes: [],
-  parameters: [],
+  determinants: [],
+  sectors: [],
   spatialScales: [],
+  sustainabilityOutcomes: [],
   temporalScales: [],
   worldRegions: [],
 };
